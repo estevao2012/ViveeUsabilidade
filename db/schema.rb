baf9b3ead2af4204a2d5bd6bd55a4f4b9bc87f9d
Estevão Andrade <estevao@uololou.com.br>
2014-11-12 00:05:56 -0200

    Alterando para postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141112001555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "avaliaca_medicos", force: true do |t|
     t.integer  "medico_id"
     t.string   "titulo"
